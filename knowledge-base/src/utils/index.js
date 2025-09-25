427b16d71a1f2e40612dcebcb51825e09aef5a4b
imgigin2003 <callmegigin@gmail.com>
2025-09-25 11:57:01 +0330

    deleted unnecessary files

--- knowledge-base/src/utils/index.js
+++
@@ -1,8 +0,0 @@
-export function createPageUrl(pageNameAndParams) {
-  const [page, params] = pageNameAndParams.split("?");
-  let url = `/${page.toLowerCase()}`;
-  if (params) {
-    url += `?${params}`;
-  }
-  return url;
-}
